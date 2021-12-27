45b9699ddfa1f1f28331840d9ab2a70de01a05be
Hyebin <thsuekfk2@naver.com>
2021-12-27 17:50:47 +0900

    뒤로가기 버튼

--- src/Detail.js
+++ src/Detail.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 
 function Detail() {
+  let history = useHistory();
+
   return (
     <div className="container">
       <div className="row">
@@ -12,7 +15,14 @@ function Detail() {
           <p>상품설명</p>
           <p>120000원</p>
           <button className="btn btn-danger">주문하기</button>
-          <button className="btn">뒤로가기</button>
+          <button
+            className="btn"
+            onClick={() => {
+              history.goBack();
+            }}
+          >
+            뒤로가기
+          </button>
         </div>
       </div>
     </div>
